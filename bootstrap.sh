de3a9bedc2d1157d7a7b29deecd4b080b0d23431
Marco Aurelio Prado dos Santos Vidoca <Marco Aurelio Prado dos Santos Vidoca>
2016-10-05 18:10:15 -0300

    Making small changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 apt-get -qqy update
+apt-get install -y apache2
+apt-get install libapache2-mod-wsgi
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
